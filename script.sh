771d60d925b81b18e5f258778d22a7689a85dddf
luke0han <lukeohanlon@outlook.com>
2023-05-30 22:49:58 +0100

    Added completion message

--- script.sh
+++ script.sh
@@ -3,3 +3,7 @@ mkdir fol_1 fol_2
 touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
 touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
 rm -rf fol_1 fol_2 fol_3
+chmod 600 fol_1/*_1.txt fol_1/*_3.txt
+chmod 700 fol_2/*_2.txt
+echo "Job completed"
+
